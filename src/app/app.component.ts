230bf14209e3db4a4362f5f6599fa214307212c5
SIMPLURIS\shassaninia <shassaninia@simpluris.com>
2017-03-17 16:30:29 -0400

    add likeMe and deleteMe functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,16 @@ export class AppComponent {
     .map(response => response.json().screenshots)
     .subscribe(res=> this.spaceScreens = res);
   }
+
+  likeMe(i){
+    if(this.spaceScreens[i].liked == 0)
+      this.spaceScreens[i].liked = 1;
+    else
+      this.spaceScreens[i].liked = 0;
+  }
+
+  deleteMe(i){
+    this.spaceScreens.splice(i,1);
+    console.log(i);
+  }
 }
